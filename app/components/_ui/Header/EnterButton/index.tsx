856a7b5fddd3e9edd8036cfc5de7ed0df592d747
CarolMarchesin <104083173+CarolMarchesin@users.noreply.github.com>
2024-11-23 13:49:00 -0300

    feat: refactor EnterButton component to use constants for button content

--- app/components/_ui/Header/EnterButton/index.tsx
+++ app/components/_ui/Header/EnterButton/index.tsx
@@ -1,15 +1,7 @@
 import Image from "next/image";
-import ProSimbolo from "@/public/pro-simbolo.svg";
-import MaisSimbolo from "@/public/mais-simbolo.svg";
 import { Popover } from "../../Popover";
 import { useState } from "react";
-
-interface ListContentButtonToEnter {
-  title: string;
-  description: string;
-  icon: string;
-  link: string;
-}
+import { listContentButtonEnter } from "@/constants/listContentEnterButton";
 
 export const EnterButton = () => {
   const [activeItem, setActiveItem] = useState<String | null>();
@@ -22,36 +14,22 @@ export const EnterButton = () => {
     setActiveItem(null);
   };
 
-  const listContentButtonToEnter: ListContentButtonToEnter[] = [
-    {
-      title: "ERP Conta Azul Pro",
-      description: "O ERP em nuvem que simplifica sua gestão financeira",
-      icon: ProSimbolo,
-      link: "https://login.contaazul.com/?_gl=1*1qqo7t0*_gcl_au*NTQ5MzkyODM4LjE3MzE2MDc0NDg.*_ga*MTQzOTI2NjIyMy4xNzMxNjA3NDQy*_ga_0ZF31QJEMG*MTczMjMxMTY5OC4xNy4xLjE3MzIzMTIxNjcuNjAuMC4w#/",
-    },
-    {
-      title: "Conta Azul Mais",
-      description: "O que sua contabilidade precisa em um só lugar",
-      icon: MaisSimbolo,
-      link: "https://mais.contaazul.com/?_gl=1*98uwvz*_gcl_au*NTQ5MzkyODM4LjE3MzE2MDc0NDg.*_ga*MTQzOTI2NjIyMy4xNzMxNjA3NDQy*_ga_0ZF31QJEMG*MTczMjMxMTY5OC4xNy4xLjE3MzIzMTIxODEuNDYuMC4w#/login",
-    },
-  ];
-
   return (
     <>
       <Popover
         buttonPopover={
           <button
-            className={`text-white inline-flex items-center text-sm/10 bg-blue-500 h-[40px] px-7 rounded font-medium`}
+            className={`text-white items-center text-sm/10 bg-blue-500 h-[40px] px-7 rounded font-medium hidden lg:inline-flex`}
           >
             Entrar
           </button>
         }
+        direction="right"
         isOpenPopoverPanel={activeItem === "Entrar"}
         onMouseEnter={handleMouseEnter}
         onMouseLeave={handleMouseLeave}
       >
-        {listContentButtonToEnter.map((item, index) => (
+        {listContentButtonEnter.map((item, index) => (
           <a key={index} href={item.link} target="_blank">
             <div className="grid grid-cols-[auto,1fr] items-center gap-4 w-[190px] leading-3">
               <Image src={item.icon} alt="Pro" width={20} height={20} />
@@ -63,7 +41,7 @@ export const EnterButton = () => {
               </div>
             </div>
 
-            {index < listContentButtonToEnter.length - 1 && (
+            {index < listContentButtonEnter.length - 1 && (
               <hr className="my-4" />
             )}
           </a>
